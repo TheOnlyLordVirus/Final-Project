442a03c88f01b4e624628c7b9b221425c0474dd0
CMF03 <80782368+CMF03@users.noreply.github.com>
2021-04-18 22:54:43 -0500

    Delete final_game directory

--- final_game/src/start/SceneInterface.java
+++
@@ -1,9 +0,0 @@
-package start;
-
-public interface SceneInterface 
-{
-	public String getName();
-	public String getDescription();
-	public void ParseCombination(String s);
-	public void interaction();
-}
